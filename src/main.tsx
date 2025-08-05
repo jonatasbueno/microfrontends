bde3ad92c21b363646058df5e16f50f3710b1f17
Jônatas Livramento <jonatas.livramento@gmail.com>
2025-08-05 16:12:45 -0300

    feat: configured chakra-iu

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,20 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router";
+import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
 
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="details" element={<App />} />
-        <Route path="dashboard" element={<App />} />
-      </Routes>
+      <ChakraProvider value={defaultSystem}>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="details" element={<App />} />
+          <Route path="dashboard" element={<App />} />
+        </Routes>
+      </ChakraProvider>
     </BrowserRouter>
   </StrictMode>
 );
