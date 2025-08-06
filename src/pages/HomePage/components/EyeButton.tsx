f412134b26b50de86234e21a63cd475ebcc35b16
Jônatas Livramento <jonatas.livramento@gmail.com>
2025-08-06 04:17:25 -0300

    feat: Update EyeButton component

--- src/pages/HomePage/components/EyeButton.tsx
+++ src/pages/HomePage/components/EyeButton.tsx
@@ -1,14 +1,19 @@
-import { Button } from "@chakra-ui/react/button";
+import { Button, type ButtonProps } from "@chakra-ui/react/button";
 import { faEye } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-type EyeButtonProps = {
+type EyeButtonProps = ButtonProps & {
   handleClick: () => void;
 };
 
-export function EyeButton({ handleClick }: EyeButtonProps) {
+export function EyeButton({ handleClick, ...props }: EyeButtonProps) {
   return (
-    <Button bg="primary" onClick={handleClick} borderRadius={"full"}>
+    <Button
+      bg="primary.500"
+      {...props}
+      onClick={handleClick}
+      borderRadius={"full"}
+    >
       <FontAwesomeIcon icon={faEye} />
     </Button>
   );
